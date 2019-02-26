dc93aa991b084a22985343c23c404eb4c6d18fd5
Tobias Cabrera Cano <tobias.cabrera@gmx.de>
2019-02-26 20:38:11 +0100

    changed url

--- src/app/vehicles/vehicle.service.ts
+++ src/app/vehicles/vehicle.service.ts
@@ -1,10 +1,10 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 import {Vehicle, VehicleRequest, VehicleStatus} from './vehicles.model';
-import { Parcel } from '../parcels/parcel.model';
-import { ReplaySubject, Subject } from 'rxjs';
-import { ParcelService } from '../parcels/parcel.service';
-import { UserService } from '../user/user.service';
+import {Parcel} from '../parcels/parcel.model';
+import {ReplaySubject, Subject} from 'rxjs';
+import {ParcelService} from '../parcels/parcel.service';
+import {UserService} from '../user/user.service';
 
 @Injectable({
     providedIn: 'root'
@@ -15,8 +15,8 @@ export class VehicleService {
     vehicleRequest: Vehicle[];
     private vehicleRequestSubject: Subject<Vehicle[]> = new ReplaySubject<Vehicle[]>(25);
     private lastVehicleRequest: Vehicle;
-    //public vehicleRequestUrl = 'https://carrierserver.cabreracano.de/vehicleRequests/';
-     public vehicleRequestUrl = 'http://localhost:8081/vehicleRequests/';
+    public vehicleRequestUrl = 'https://carrierserver.cabreracano.de/vehicleRequests/';
+    // public vehicleRequestUrl = 'http://localhost:8081/vehicleRequests/';
     private allVehicleRequestsSubject: Subject<Vehicle[]> = new ReplaySubject<Vehicle[]>(25);
 
     constructor(private httpClient: HttpClient, private parcelService: ParcelService, private userService: UserService) {
@@ -31,7 +31,7 @@ export class VehicleService {
         }
         this.lastVehicleRequest = vehicleRequest;
         return this.httpClient.post(this.vehicleRequestUrl, JSON.stringify(vehicleRequest), {
-            headers: { userToken: this.userToken, identifier: 'APP', 'Content-Type': 'application/json' }
+            headers: {userToken: this.userToken, identifier: 'APP', 'Content-Type': 'application/json'}
         }).subscribe(
             (response: Response) => {
                 console.log(this.vehicleRequest);
@@ -50,7 +50,7 @@ export class VehicleService {
 
     public getVehicleRequestById(vehicleRequest_id: number) {
         this.httpClient.get<Vehicle[]>(this.vehicleRequestUrl + vehicleRequest_id, {
-            headers: { userToken: this.userToken, identifier: 'APP' }
+            headers: {userToken: this.userToken, identifier: 'APP'}
         }).subscribe((ps) => {
             this.vehicleRequestSubject.next(ps);
         });
@@ -60,18 +60,18 @@ export class VehicleService {
 
     public cancelVehicleRequest(parcelObject: Parcel) {
         this.getVehicleRequestById(parcelObject.vehicleRequestId).subscribe(data => {
-            // TODO write method getVehicleRequestById, first get vehicle request and in response set status
-            this.vehicleRequest = data;
-            // Set Cancelation status for vehicle request
-            this.vehicleRequest['status'] = VehicleStatus.CANCELED_BY_USER;
-            // Cancel Vehicle Request
-            this.httpClient.put(this.vehicleRequestUrl + parcelObject.vehicleRequestId, JSON.stringify(this.vehicleRequest), {
-                headers: { identifier: 'APP', 'Content-Type': 'application/json' }
-            }).subscribe(
-                (response: Response) => {
-                    console.log('Vehicle Request is cancelled');
-                });
-        }
+                // TODO write method getVehicleRequestById, first get vehicle request and in response set status
+                this.vehicleRequest = data;
+                // Set Cancelation status for vehicle request
+                this.vehicleRequest['status'] = VehicleStatus.CANCELED_BY_USER;
+                // Cancel Vehicle Request
+                this.httpClient.put(this.vehicleRequestUrl + parcelObject.vehicleRequestId, JSON.stringify(this.vehicleRequest), {
+                    headers: {identifier: 'APP', 'Content-Type': 'application/json'}
+                }).subscribe(
+                    (response: Response) => {
+                        console.log('Vehicle Request is cancelled');
+                    });
+            }
         );
 
 
@@ -79,7 +79,7 @@ export class VehicleService {
 
     public reloadVehicleRequests() {
         this.httpClient.get<Vehicle[]>(this.vehicleRequestUrl + 'users/' + this.userService.getUserToken(), {
-            headers: { userToken: this.userService.getUserToken(), identifier: "APP" }
+            headers: {userToken: this.userService.getUserToken(), identifier: 'APP'}
         }).subscribe((ps) => {
             console.log(ps);
             this.allVehicleRequestsSubject.next(ps);
