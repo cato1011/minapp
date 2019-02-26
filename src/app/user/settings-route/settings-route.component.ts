e376c370fc8a8073b6a667916b50273820b22e35
Tobias Cabrera Cano <tobias.cabrera@gmx.de>
2019-02-26 22:33:09 +0100

    set mock user for testing

--- src/app/user/settings-route/settings-route.component.ts
+++ src/app/user/settings-route/settings-route.component.ts
@@ -1,9 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import {UserService} from '../user.service';
 import {DeliveryPlacesService} from '../../delivery-places/delivery-places.service';
 import {Observable} from 'rxjs';
 import {DeliveryPlace} from '../../delivery-places/delivery-places.model';
+import {MOCK_USER} from '../user.model';
 
 @Component({
     selector: 'app-settings-route',
@@ -16,8 +17,8 @@ export class SettingsRouteComponent implements OnInit {
     preferedDeliveryPlaceId: number;
     deliveryPlaces$: Observable<DeliveryPlace[]>;
     preferedDeliveryPlace$: Observable<DeliveryPlace>;
-    preferedDeliveryPlaceExists: boolean = false;
-    emailAddress:string;
+    preferedDeliveryPlaceExists = false;
+    emailAddress: string;
 
     constructor(private translateService: TranslateService,
                 private deliveryPlacesService: DeliveryPlacesService,
@@ -25,20 +26,22 @@ export class SettingsRouteComponent implements OnInit {
     }
 
     ngOnInit() {
+        this.userService.setUser(MOCK_USER);
         this.initData();
     }
 
     initData() {
-        this.emailAddress=this.userService.getEmail();
-        this.currentLang = this.translateService.currentLang;
         this.deliveryPlacesService.reloadDeliveryPlaces();
         this.deliveryPlaces$ = this.deliveryPlacesService.getDeliveryPlaces();
-        this.userService.getSettings().subscribe(settings => {
-            if (settings.preferedDeliveryPlaceId) {
+        this.userService.getUser().subscribe(user => {
+            this.emailAddress = user.email;
+            this.currentLang = user.userConfiguration.applicationLangauge;
+            if (user.userConfiguration.preferedDeliveryPlaceId) {
                 this.preferedDeliveryPlaceExists = true;
-                this.deliveryPlacesService.loadPreferedDeliveryPlace(settings.preferedDeliveryPlaceId);
+                this.deliveryPlacesService.loadPreferedDeliveryPlace(user.userConfiguration.preferedDeliveryPlaceId);
                 this.preferedDeliveryPlace$ = this.deliveryPlacesService.getPreferedDeliveryPlace();
-                this.preferedDeliveryPlaceId = parseInt(settings.preferedDeliveryPlaceId);
+                this.preferedDeliveryPlaceId = parseInt(user.userConfiguration.preferedDeliveryPlaceId);
+
             } else {
                 this.preferedDeliveryPlaceExists = false;
             }
